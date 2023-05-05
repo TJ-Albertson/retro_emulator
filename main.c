5b705ca3d2321c6e1315a9dc9f799530533a6626
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2023-05-05 10:23:25 -0500

    redner

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 // Constants for the window size and box dimensions
 const int SCREEN_WIDTH = 1280;
@@ -10,29 +10,167 @@ const int SCREEN_HEIGHT = 720;
 const int REG_BOX_WIDTH = 200;
 const int REG_BOX_HEIGHT = 50;
 
-void create_register(SDL_Renderer* renderer, int x, int y, char* value, TTF_Font* font, int width, int height) {
+SDL_Texture* register_textures[4];
+SDL_Texture* rom_textures[16];
+SDL_Texture* ram_textures[16];
+
+uint8_t registers[5];
+
+
+uint8_t A = 0x00;
+uint8_t X = 0x01;
+uint8_t Y = 0x02;
+uint8_t P = 0x03;
+uint16_t PC = 0x0000;
+
+uint8_t ROM[32000] = { 0 };
+uint8_t RAM[32000] = { 0 };
+
+void create_surface(SDL_Renderer* renderer, TTF_Font* font)
+{
+    SDL_Color color = { 0, 0, 0, 255 };
+
+    char hex_string[5];
+
+    sprintf_s(hex_string, sizeof(hex_string), "%04x", PC);
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, hex_string, color);
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    register_textures[4] = texture;
+
+    SDL_FreeSurface(surface);
+}
+
+void create_dynamic_textures(SDL_Renderer* renderer, TTF_Font* font)
+{
+
+    
+    for (int i = 0; i < 4; i++) {
+        SDL_Color color = { 0, 0, 0, 255 };
+
+        char hex_string[3];
+
+        sprintf_s(hex_string, sizeof(hex_string), "%02x", registers[i]);
+
+        SDL_Surface* surface = TTF_RenderText_Solid(font, hex_string, color);
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+        register_textures[i] = texture;
+
+        SDL_FreeSurface(surface);
+    }
+        
+    SDL_Color color = { 0, 0, 0, 255 };
+
+    char hex_string[5];
+
+    sprintf_s(hex_string, sizeof(hex_string), "%04x", PC);
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, hex_string, color);
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    register_textures[4] = texture;
+
+    SDL_FreeSurface(surface);
+
+
+    /*
+    for (int i = 0; i < 15; i++) {
+        SDL_Color color = { 0, 0, 0, 255 };
+
+        char hex_string[3];
+
+        sprintf(hex_string, "%02x", rom_textures[i]);
+
+        SDL_Surface* surface = TTF_RenderText_Solid(font, hex_string, color);
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+        rom_textures[i] = texture;
+
+        SDL_FreeSurface(surface);
+    }
+    */
+}
+
+// rom/ram/register values
+void render_dyanmic_textures(SDL_Renderer* renderer, TTF_Font* font)
+{
+
+    // render register textures
+    int startPosY = 150;
+    for (int i = 0; i < 4; i++) {
+        int tex_width, tex_height;
+        SDL_QueryTexture(register_textures[i], NULL, NULL, &tex_width, &tex_height);
+        SDL_Rect rect = { 1000, startPosY, tex_width, tex_height };
+        SDL_RenderCopy(renderer, register_textures[i], NULL, &rect);
+        //SDL_DestroyTexture(register_textures[i]);
+        startPosY += 100;
+    }
+
+    int tex_width, tex_height;
+    SDL_QueryTexture(register_textures[4], NULL, NULL, &tex_width, &tex_height);
+    SDL_Rect rect = { 1000, startPosY, tex_width, tex_height };
+    SDL_RenderCopy(renderer, register_textures[4], NULL, &rect);
+    // SDL_DestroyTexture(register_textures[i]);
+ 
+
+
+    /*
+    // render rom textures
+    startPosY = 150;
+    for (int i = 0; i < 15; i++) {
+        int tex_width, tex_height;
+        SDL_QueryTexture(rom_textures[i], NULL, NULL, &tex_width, &tex_height);
+        SDL_Rect rect = { 50, startPosY, tex_width, tex_height };
+        SDL_RenderCopy(renderer, rom_textures[i], NULL, &rect);
+        //SDL_DestroyTexture(rom_textures[i]);
+        startPosY += 20;
+    }
+
+    // render ram textures
+    startPosY = 150;
+    for (int i = 0; i < 15; i++) {
+        int tex_width, tex_height;
+        SDL_QueryTexture(ram_textures[i], NULL, NULL, &tex_width, &tex_height);
+        SDL_Rect rect = { 525, startPosY, tex_width, tex_height };
+        SDL_RenderCopy(renderer, ram_textures[i], NULL, &rect);
+        //SDL_DestroyTexture(ram_textures[i]);
+        startPosY += 20;
+    }
+    */
+}
+
+void destroy_dynamic_textures()
+{
+}
+
+void create_register(SDL_Renderer* renderer, int x, int y, char* value, TTF_Font* font, int width, int height)
+{
 
     // value -> surface -> texture
 
     // texture & rect = render
-    
+
     SDL_Color color = { 0, 0, 0, 255 };
 
     SDL_Surface* surface = TTF_RenderText_Solid(font, value, color);
 
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface); 
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    //rect just 
+    // rect just
     SDL_Rect rect = { 0, 0, surface->w, surface->h };
 
-    SDL_RenderCopy(renderer, texture, NULL, &rect); 
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
 
-    SDL_FreeSurface(surface); 
-
-    SDL_DestroyTexture(texture); 
+    SDL_FreeSurface(surface);
+    // SDL_DestroyTexture(texture);
 }
 
-// Helper function to create a box with a label
 void create_box(SDL_Renderer* renderer, int x, int y, char* label, TTF_Font* font, int width, int height)
 {
     // Set the box color to gray
@@ -51,6 +189,8 @@ void create_box(SDL_Renderer* renderer, int x, int y, char* label, TTF_Font* fon
     // Create a texture from the surface
     SDL_Texture* texture_label = SDL_CreateTextureFromSurface(renderer, surface_label);
 
+    SDL_FreeSurface(surface_label);
+
     // Get the dimensions of the texture
     int tex_width, tex_height;
     SDL_QueryTexture(texture_label, NULL, NULL, &tex_width, &tex_height);
@@ -65,7 +205,7 @@ void create_box(SDL_Renderer* renderer, int x, int y, char* label, TTF_Font* fon
     SDL_RenderCopy(renderer, texture_label, NULL, &label_rect);
 
     // Free the surface and texture
-    SDL_FreeSurface(surface_label);
+
     SDL_DestroyTexture(texture_label);
 }
 
@@ -86,18 +226,17 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-
     // Create a window
     SDL_Window* window = SDL_CreateWindow("Box Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-
     // Clear the renderer with white
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
+    // Static renders
     // Create boxes and labels
     create_box(renderer, 1000, 150, "A Register", font, REG_BOX_WIDTH, REG_BOX_HEIGHT);
     create_box(renderer, 1000, 250, "X Register", font, REG_BOX_WIDTH, REG_BOX_HEIGHT);
@@ -108,28 +247,45 @@ int main(int argc, char* argv[])
     create_box(renderer, 50, 150, "ROM", font, 400, 450);
 
     // Render the scene
-    SDL_RenderPresent(renderer);
+    create_dynamic_textures(renderer, font);
+
+    registers[0] = 0x00;
+    registers[1] = 0x01;
+    registers[2] = 0x02;
+    registers[3] = 0x03;
+    registers[4] = 0x04;
+
 
     // Wait for a quit event
     bool quit = false;
     while (!quit) {
-
 
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
-            if (event.type == SDL_QUIT) {
-                quit = true;
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_RIGHT) {
+                    SDL_DestroyTexture(register_textures[4]);
+                    printf("Right arrow key pressed\n");
+                    PC+=0x0001;
+                    create_surface(renderer, font);
+                    printf("PC: %04x\n", PC);
+                }
             }
         }
 
         // Loop
 
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
 
-        // Render
-        
+
+        // Dynamic Renders
+        render_dyanmic_textures(renderer, font);
+
+        SDL_RenderPresent(renderer);
     }
 
     // Clean up
